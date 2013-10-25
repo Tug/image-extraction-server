327abb630414b243b454ce91942eb90e0fef38dc
Tug <dekervit@gmail.com>
2013-10-25 15:40:46 +0200

    use sqlite

--- imageex/settings.py
+++ imageex/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'imageex', # Or path to database file if using sqlite3.
         'USER': 'imageex', # Not used with sqlite3.
         'PASSWORD': 'password', # Not used with sqlite3.
